b1a1fe9ae5d9ca8401231d23341683b96cdb069c
daniheo <heo10@illinois.edu>
2019-12-09 16:03:52 -0600

    edit #3

--- app/src/main/java/com/example/cs_final_project2019/SaveActivity.java
+++ app/src/main/java/com/example/cs_final_project2019/SaveActivity.java
@@ -23,46 +23,5 @@ import java.util.ArrayList;
 public class SaveActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_screen);
-
-        Button backButton = findViewById(R.id.backButton);
-
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SaveActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button sendButton = findViewById(R.id.sendButton);
-
-        sendButton.setEnabled(false);
-        if (checkPermission(Manifest.permission.SEND_SMS)) {
-            sendButton.setEnabled(true);
-        } else {
-            ActivityCompat.requestPermissions(SaveActivity.this, new String[] {Manifest.permission.SEND_SMS}, 1);
-        }
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendSMS();
-            }
-        });
-
-        EditText phoneNumView = findViewById(R.id.phoneNumView);
-        phoneNumView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                phoneNumView.setText("");
-            }
-        });
-    }
-
-    public boolean checkPermission(String check) {
-        int seek = ContextCompat.checkSelfPermission(this, check);
-        return (seek == PackageManager.PERMISSION_GRANTED);
     }
 }
