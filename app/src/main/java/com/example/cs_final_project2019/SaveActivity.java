5639556ea9fca6594904d754dc870810ca8299a1
davetd2 <55113228+davetd2@users.noreply.github.com>
2019-12-09 16:08:39 -0600

    Update SaveActivity.java

--- app/src/main/java/com/example/cs_final_project2019/SaveActivity.java
+++ app/src/main/java/com/example/cs_final_project2019/SaveActivity.java
@@ -21,7 +21,46 @@ import androidx.core.content.ContextCompat;
 import java.util.ArrayList;
 
 public class SaveActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_screen);
+
+        Button sendButton = findViewById(R.id.sendButton);
+
+        sendButton.setEnabled(false);
+        if (checkPermission(Manifest.permission.SEND_SMS)) {
+            sendButton.setEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(SaveActivity.this, new String[] {Manifest.permission.SEND_SMS}, 1);
+        }
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText phoneNumView = findViewById(R.id.phoneNumView);
+                String phoneNo = phoneNumView.getText().toString();
+                String sms = getIntent().getStringExtra("password");
+
+                try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS Sent!",
+                            Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS failed, please try again later!",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    public boolean checkPermission(String check) {
+        int seek = ContextCompat.checkSelfPermission(this, check);
+        return (seek == PackageManager.PERMISSION_GRANTED);
     }
 }
